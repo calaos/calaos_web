211c26d91daaf5ae90efcc810701093adcccda6a
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-04-11 23:36:36 +0200

    Fix hostname check and let rename the file only once read is finished.

--- config_update.php
+++ config_update.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 
 <?php 
+
 if (!file_exists("/tmp/local_config.xml"))
 {
   echo "Nothing to do; exiting";
@@ -16,32 +17,31 @@ if (!$xml->open("/tmp/local_config.xml"))
 }
 
 
+if (!file_exists(getenv("HOME") + "/.config/calaos/"))
+{
+  echo "Creating " . getenv("HOME") . "/.config/calaos/\n";
+  mkdir(getenv("HOME") . "/.config/calaos/", 0777, true);
+}
+
+if (!rename("/tmp/local_config.xml", getenv("HOME") . "/.config/calaos/local_config.xml"))
+{
+  echo "Error renaming local_config.xml file\n";
+  return -1;
+}
+
 while($xml->read())
 {
   if ($xml->name == "calaos:option")
   {
-    if ($xml->getAttribute("name") == "hostame")
+    if ($xml->getAttribute("name") == "hostname")
     {
-      if (!file_put_contents("/etc/hostname", $xml->getAttribute("value")))
+      if (!file_put_contents("/etc/hostname", $xml->getAttribute("value") . "\n"))
       {
         echo "Error writing hostname\n";
         return -1;
       }
     }
   }
-}
-
-
-if (!file_exists(getenv("HOME") + "/.config/calaos/")
-{
-  echo "Creating " + getenv("HOME") +   "/.config/calaos/";
-  mkdir(getenv("HOME") + "/.config/calaos/", 0777, true);
-}
-
-if (!rename("/tmp/local_config.xml", getenv("HOME") + "/.config/calaos/local_config.xml"))
-{
-  echo "Error renaming local_config.xml file\n";
-  return -1;
 }
 
 echo "Finish!\n"
