671304981ef4f1ea8b1fc432a7462817a1d86abf
Raoul Hecky <raoul.hecky@gmail.com>
2013-07-26 10:32:44 +0200

    Handle correctly http OPTIONS requests for CORS

--- api.php
+++ api.php
@@ -45,6 +45,19 @@
                 header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
                 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
         }
+
+        //handle the OPTIONS request for CORS
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
+        {
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+                exit();
+        }
+
         header("Content-type: application/json");
 
         if ($jdata == NULL)
